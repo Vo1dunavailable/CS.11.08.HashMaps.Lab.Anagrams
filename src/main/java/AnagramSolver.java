0053c631291ae2dbbd8d61bc7dd0f50619a9398d
vo1d <voidunavailable1@gmail.com>
2025-04-07 09:53:29 +0800

    done

--- src/main/java/AnagramSolver.java
+++ src/main/java/AnagramSolver.java
@@ -1,9 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class AnagramSolver {
 
     private AnagramSolver() {};
+
+    private static ArrayList<String> readFile(String filename) throws FileNotFoundException {
+        File myFile = new File(filename);
+        Scanner myScanner = new Scanner(myFile);
+        ArrayList<String> words = new ArrayList<>();
+
+        while (myScanner.hasNextLine()) {
+            String word = myScanner.nextLine();
+            words.add(word);
+        }
+
+        myScanner.close();
+
+        return words;
+    }
+
+    private static String bubbleSort(String word) {
+        char[] wordArray = word.toCharArray();
+        char swap;
+
+        for (int i = 0; i < wordArray.length - 1; i++) {
+            for (int j = 0; j < wordArray.length - i - 1; j++) {
+                if (wordArray[j] > wordArray[j + 1]) {
+                    swap = wordArray[j];
+                    wordArray[j] = wordArray[j + 1];
+                    wordArray[j + 1] = swap;
+                }
+            }
+        }
+
+        return String.valueOf(wordArray);
+    }
 
     /**
      * Input: name of text file (containing English words).
@@ -11,8 +46,24 @@ public class AnagramSolver {
      * @param filename
      * @return
      */
-    public static HashMap<String, ArrayList<String>> anagrams(String filename) {
-        return null;
+    public static HashMap<String, ArrayList<String>> anagrams(String filename) throws FileNotFoundException {
+        ArrayList<String> words = readFile(filename);
+        HashMap<String, ArrayList<String>> anagrams = new HashMap<>();
+        String key;
+        ArrayList<String> currentset;
+
+        for (String w: words) {
+            key = bubbleSort(w);
+            currentset = anagrams.get(key);
+            if (currentset == null) {
+                currentset = new ArrayList<>();
+            }
+
+            currentset.add(w);
+            anagrams.put(key, currentset);
+        }
+
+        return anagrams;
     }
 
     /**
@@ -22,7 +73,16 @@ public class AnagramSolver {
      * @return
      */
     public static ArrayList<String> mostFrequentAnagram(HashMap<String, ArrayList<String>> anagrams) {
-        return null;
+        int maxSize = 0;
+        ArrayList<String> maxsizevalue = new ArrayList<>();
+        for (ArrayList<String> value: anagrams.values()) {
+            if (value.size() > maxSize) {
+                maxSize = value.size();
+                maxsizevalue = value;
+            }
+        }
+
+        return maxsizevalue;
     }
 
     /**
@@ -31,7 +91,12 @@ public class AnagramSolver {
      * @param anagrams
      */
     public static void printKeyValuePairs(HashMap<String, ArrayList<String>> anagrams) {
-
+        ArrayList<String> value;
+        for (String key: anagrams.keySet()) {
+            value = anagrams.get(key);
+            System.out.print(key + ": ");
+            System.out.println(value);
+        }
     }
 
 }
